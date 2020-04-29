68b6a50b21337166f09aec44bfed72d3669acf48
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-29 10:36:12 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,20 @@
 import pytest
-import src.exercise
+from src.money import Money
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    a = Money(10,0)
+    b = Money(5,0)
+
+    assert str(a.plus(b)) == "15.00p"
+
+    a = Money(10, 0)
+    b = Money(3, 0)
+    c = Money(5, 0)
+
+    assert not a.less_than(b)
+    assert b.less_than(c)
+
+    a = Money(10, 0)
+    b = Money(3, 50)
+
+    assert str(a.minus(b)) == "6.50p"
